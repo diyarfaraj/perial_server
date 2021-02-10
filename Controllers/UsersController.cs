f0f3f70e37e10f6875f189752dd5fb971b81d45f
Diyar Faraj <diyar.faraj@gmail.com>
2021-02-10 23:03:51 +0100

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using perial_server.Data;
+using perial_server.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace perial_server.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+        [HttpGet("{id}")]
+
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            
+            return user;
+        }
+    }
+}
+
